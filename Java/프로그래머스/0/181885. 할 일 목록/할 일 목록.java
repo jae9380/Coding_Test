11f1e1b1584243aeb497b413c9b6fe858449a1b1
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-03-20 18:27:11 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i<todo_list.length; i++) {
+            if (!finished[i]) arr.add(todo_list[i]);
+        }
+        String[] answer = arr.toArray(new String[arr.size()]);
+        return answer;
+    }
+}
