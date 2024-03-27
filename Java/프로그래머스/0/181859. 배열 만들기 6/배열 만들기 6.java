d30cf0589ee4007dca9c2dd9a3fc2cc263f999b1
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-03-27 11:51:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 19.75 ms, Memory: 75.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,26 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < arr.length ; i++) {
-            if (!list.isEmpty()) {
-                if (list.get(list.size()-1) != arr[i]) {
-                    list.add(arr[i]);
-                }else {
-                    list.remove(list.size()-1);
-                }
-
-            }else {
-                list.add(arr[i]);
-            }
-        }
-        
-        int[] answer = list.isEmpty() ? new int[]{-1} : list.stream()
-                .mapToInt(i->i)
-                .toArray();
-        
+        int[] answer = {};
         return answer;
     }
 }
