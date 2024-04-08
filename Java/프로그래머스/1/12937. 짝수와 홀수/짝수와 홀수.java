9430770e9f86c30e4a3e48c152f8b4a70b30c77c
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-04-08 22:33:59 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String func(int number) {
+        return number%2==0 ? "Even" : "Odd";
+    }
+    public String solution(int num) {
+        return func(num);
+    }
+}
