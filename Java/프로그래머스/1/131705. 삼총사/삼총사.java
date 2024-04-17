4547baaf504a27ac50a3cb2afc14ebcbd328a1e8
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-04-17 21:49:32 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i=0;i<number.length-2;i++) {
+            for(int j=i+1;j<number.length-1;j++) {
+                for(int h=j+1;h<number.length;h++){
+                    answer = number[i]+number[j]+number[h] == 0 ? answer+1 : answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
