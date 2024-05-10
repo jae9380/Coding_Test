07f28fcf746df39de8fbad3dbbb38ec273bca2fe
jae9380 <145907325+jae9380@users.noreply.github.com>
2024-05-10 13:28:05 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n;
+        int originCnt = Integer.bitCount(n);
+        
+        while(true){
+            int oneCnt = Integer.bitCount(++answer);
+            
+            if(oneCnt == originCnt) break;
+        }
+        
+        return answer;
+    }
+}
